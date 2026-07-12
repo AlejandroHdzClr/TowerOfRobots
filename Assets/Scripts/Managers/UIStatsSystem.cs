1debd5c534dbda0e34eb3e39cabf867dd042c8be
AlejandroHdzClr <alehdzclr@gmail.com>
2026-07-12 22:44:19 +0100

    HealthBar Implemented

---
+++ Assets/Scripts/Managers/UIStatsSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class UIStatsSystem : MonoBehaviour
+    {
+        [SerializeField] private PlayerHealthSystem playerHealthSystem;
+        [SerializeField] private Image healthBarImage;
+
+        private void OnEnable()
+        {
+            playerHealthSystem.HealthHasChanged += PlayerHealthSystemOnHealthHasChanged;
+        }
+        
+        private void OnDisable()
+        {
+            playerHealthSystem.HealthHasChanged -= PlayerHealthSystemOnHealthHasChanged;
+        }
+
+        private void PlayerHealthSystemOnHealthHasChanged(float obj)
+        {
+            Debug.Log($"[{gameObject.name} - InstanceID: {GetInstanceID()}] healthBarImage es null: {healthBarImage == null}", this);
+            healthBarImage.fillAmount = obj;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerHealthSystem.cs
+++ Assets/Scripts/Player/PlayerHealthSystem.cs
@@ -9,10 +9,17 @@ namespace Player
         [SerializeField] private float damageTime;
         private float currentTime;
         private bool wasDamaged;
+        public event Action<float> HealthHasChanged; 
         protected override void Awake()
         {
             base.Awake();
             main.CurrentEnergy = main.MaxEnergy;
+            HealthHasChanged?.Invoke(HealthChanged());
+        }
+
+        private float HealthChanged()
+        {
+            return main.CurrentEnergy / main.MaxEnergy;
         }
 
         public void TakeDamage(float damage)
@@ -29,9 +36,9 @@ namespace Player
                 {
                     Debug.Log($"Player hitteado, le queda {main.CurrentEnergy} de energia");
                 }
-
                 wasDamaged = true;
                 currentTime = 0f;
+                HealthHasChanged?.Invoke(HealthChanged());
             }
         }
 
